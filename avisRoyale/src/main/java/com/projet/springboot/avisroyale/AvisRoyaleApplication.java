1d764a7aef9c337630eaf7d9658f1e0b7d2b9a97
Kindy <2201937@crosemont.qc.ca>
2024-06-07 00:18:31 -0400

    Création projet.

---
+++ avisRoyale/src/main/java/com/projet/springboot/avisroyale/AvisRoyaleApplication.java
@@ -0,0 +1,13 @@
+package com.projet.springboot.avisroyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisRoyaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisRoyaleApplication.class, args);
+	}
+
+}
---
+++ avisRoyale/src/test/java/com/projet/springboot/avisroyale/AvisRoyaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.springboot.avisroyale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisRoyaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
